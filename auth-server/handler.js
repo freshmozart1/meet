ecc8ac43bd6f632eb97d157b91b965211bf72906
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-21 20:29:17 +0200

    Added serverless API endponts for authorization and retrieval of the events.

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -2,8 +2,8 @@ import { google } from 'googleapis';
 
 const calendar = google.calendar('v3');
 const SCOPES = ['https://www.googleapis.com/auth/calendar.events.public.readonly'];
-const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID, REDIRECT_URIS } = process.env;
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URIS[0]);
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID, REDIRECT_URI } = process.env;
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
 export async function getAuthURL() {
   return {
@@ -18,3 +18,58 @@ export async function getAuthURL() {
     }))
   }
 };
+
+export async function getAccessToken(event) {
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) return reject(error);
+      return resolve(response);
+    });
+  }).then((results) => {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify(results)
+    };
+  }).catch((error) => {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    };
+  });
+}
+
+export async function getCalendarEvents(event) {
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  oAuth2Client.setCredentials({ access_token });
+  return new Promise((resolve, reject) => {
+    calendar.events.list({
+      calendarId: CALENDAR_ID,
+      auth: oAuth2Client,
+      timeMin: new Date().toISOString(),
+      singleEvents: true,
+      orderBy: 'startTime'
+    }, (error, response) => {
+      if (error) return reject(error);
+      return resolve(response);
+    });
+  }).then((results) => {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify(results.data.items)
+    };
+  }).catch((error) => {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    };
+  });
+}
