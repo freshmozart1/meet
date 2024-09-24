08d8f2317cbe272be5de4b96439a856afc84be5b
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-24 12:51:40 +0200

    Added error messages to ignore.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const MESSAGES_TO_IGNORE = ['Warning: `ReactDOMTestUtils.act` is deprecated in favor of `React.act`. Import `act` from `react` instead of `react-dom/test-utils`. See https://react.dev/warnings/react-dom-test-utils for more info.'];
+
+const originalError = console.error.bind(console.error);
+
+console.error = (...args) => {
+    const ignoreMessage = MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
+    if (!ignoreMessage) originalError(...args);
+}
