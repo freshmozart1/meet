adbb21096da7167ddb57cb5c4488123c431e27f1
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-24 17:14:16 +0200

    added act.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,14 +3,21 @@
 import { render } from '@testing-library/react';
 
 import App from '../App';
+import { act } from 'react';
 
 describe('<App /> component', () => {
-    test('renders list of events', () => {
-        // eslint-disable-next-line testing-library/no-node-access
-        expect(render(<App />).container.querySelector('#event-list')).toBeInTheDocument();
+    test('renders list of events', async () => {
+        const { container } = render(<App />);
+        await act(async () => {
+            // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+            expect(container.querySelector('#event-list')).toBeInTheDocument();
+        });
     });
-    test('render CitySearch', () => {
-        // eslint-disable-next-line testing-library/no-node-access
-        expect(render(<App />).container.querySelector('#city-search')).toBeInTheDocument();
+    test('render CitySearch', async () => {
+        const { container } = render(<App />);
+        await act(async () => {
+            // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+            expect(container.querySelector('#city-search')).toBeInTheDocument();
+        });
     });
 });
