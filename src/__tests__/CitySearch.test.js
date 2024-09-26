9ca83739fed04f605ff57d1f8b9a111ed6eb6063
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-26 16:55:57 +0200

    Improved test coverage.

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -7,26 +7,30 @@ import CitySearch from '../components/CitySearch';
 import { extractLocations, getEvents } from '../api';
 
 describe('<CitySearch /> component', () => {
-    test('renders text input', () => {
+    test('has text input', () => {
         render(<CitySearch />);
         const cityTextBox = screen.queryByRole('textbox');
         expect(cityTextBox).toBeInTheDocument();
     });
-    test('suggestions list is hidden by default', () => {
+    test('has a city dropdown that is hidden by default', () => {
         render(<CitySearch />);
         const suggestionInput = screen.queryByRole('button');
         expect(suggestionInput).toHaveAttribute('aria-expanded', 'false');
     });
-    test('renders a list of suggestions when city textbox gains focus', async () => {
-        render(<CitySearch />);
+    test('shows a dropdown of all cities when city textbox gains focus', async () => {
+        const allLocations = extractLocations(await getEvents());
+        render(<CitySearch allLocations={allLocations} />);
         const user = userEvent.setup();
         const suggestionInput = screen.queryByRole('button');
         expect(suggestionInput).toHaveAttribute('aria-expanded', 'false');
         const cityTextBox = screen.getByPlaceholderText('Search for a city');
         await act(async () => await user.click(cityTextBox));
         expect(suggestionInput).toHaveAttribute('aria-expanded', 'true');
+        let cityListItems = screen.queryAllByRole('listitem');
+        expect(cityListItems).toHaveLength(allLocations.length + 1);
+        expect(screen.queryByText('No city found')).not.toBeInTheDocument();
     });
-    test('updates list of suggestions correctly when user types in city textbox', async () => {
+    test('updates list of suggested cities correctly when user types in city textbox', async () => {
         const user = userEvent.setup();
         const allEvents = await getEvents();
         const allLocations = extractLocations(allEvents);
@@ -39,8 +43,9 @@ describe('<CitySearch /> component', () => {
         for (let i = 0; i < suggestions.length; i++) {
             expect(suggestionListItems[i]).toHaveTextContent(suggestions[i]);
         }
+        expect(screen.queryByText('No city found')).not.toBeInTheDocument();
     });
-    test('renders the suggestion text in the textbox when user clicks on a suggestion', async () => {
+    test('sets the correct value of the city textbox when user clicks on a city', async () => {
         const user = userEvent.setup();
         const allEvents = await getEvents();
         const allLocations = extractLocations(allEvents);
@@ -50,5 +55,44 @@ describe('<CitySearch /> component', () => {
         const BerlinGermanySuggestion = screen.queryAllByRole('listitem')[0];
         await act(async () => await user.click(BerlinGermanySuggestion));
         expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
+    });
+    test('hides the city dropdown when user clicks on a city', async () => {
+        render(<CitySearch allLocations={extractLocations(await getEvents())} />);
+        const user = userEvent.setup();
+        const cityTextBox = screen.queryByRole('textbox');
+        await act(async () => await user.type(cityTextBox, 'Berlin'));
+        const BerlinGermanySuggestion = screen.queryAllByRole('listitem')[0];
+        await act(async () => await user.click(BerlinGermanySuggestion));
+        const suggestionInput = screen.queryByRole('button');
+        expect(suggestionInput).toHaveAttribute('aria-expanded', 'false');
+    });
+    test('hides the city dropdown when the city textbox has no value', async () => {
+        render(<CitySearch allLocations={extractLocations(await getEvents())} />);
+        const user = userEvent.setup();
+        const cityTextBox = screen.queryByRole('textbox');
+        await act(async () => await user.type(cityTextBox, 'Berlin'));
+        const suggestionInput = screen.queryByRole('button');
+        expect(suggestionInput).toHaveAttribute('aria-expanded', 'true');
+        await act(async () => await user.clear(cityTextBox));
+        expect(suggestionInput).toHaveAttribute('aria-expanded', 'false');
+    });
+    test('shows all cities in the city dropdown when the user clicks on the "See all cities" option', async () => {
+        const allLocations = extractLocations(await getEvents());
+        render(<CitySearch allLocations={allLocations} />);
+        const user = userEvent.setup();
+        await act(async () => await user.type(screen.getByRole('textbox'), 'Berlin'));
+        await act(async () => await user.click(screen.getAllByRole('listitem').pop()));
+        for (let location of allLocations) {
+            expect(screen.getAllByRole('listitem').map(item => item.textContent)).toContain(location);
+        }
+    });
+    test('show disabled "No city found" when no city matches the user input', async () => {
+        render(<CitySearch allLocations={extractLocations(await getEvents())} />);
+        const user = userEvent.setup();
+        await act(async () => await user.type(screen.getByRole('textbox'), 'Hamburg'));
+        const noCityItem = screen.getAllByRole('listitem')[0];
+        expect(noCityItem).toHaveTextContent('No city found');
+        expect(noCityItem).toHaveClass('disabled');
+        expect(screen.getByText('See all cities')).toBeInTheDocument();
     });
 });
