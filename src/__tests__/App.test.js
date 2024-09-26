db7aa09bbc8f9ae86fca779992e45f2f21153e61
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-26 12:43:25 +0200

    Added tests for rendering correct number of events to improve test coverage.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,10 @@
 // src/__tests__/App.test.js
 
-import { render, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import App from '../App';
-import React from 'react';
+import React, { act } from 'react';
+import userEvent from '@testing-library/user-event';
 
 describe('<App /> component', () => {
     test('renders list of events', async () => {
@@ -26,5 +27,32 @@ describe('<App /> component', () => {
             // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
             expect(container.querySelector('#numberOfEvents')).toBeInTheDocument();
         });
+    });
+    test('show 32 events by default', async () => {
+        const { container } = render(<App />);
+        await waitFor(() => {
+            // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+            expect(container.querySelector('#event-list').children.length).toBe(32);
+        });
+    });
+    test('render correct number of events', async () => {
+        const { container } = render(<App />);
+        const user = userEvent.setup();
+        await waitFor(() => {
+            // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+            expect(container.querySelector('#numberOfEvents')).toBeInTheDocument();
+        });
+        await waitFor(() => {
+            // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+            expect(container.querySelector('#event-list')).toBeInTheDocument();
+        });
+        const numberOfEventsInput = screen.getByLabelText('Events on page:');
+        expect(numberOfEventsInput).toHaveValue(32);
+        await act(async () => {
+            await user.clear(numberOfEventsInput);
+            await user.type(numberOfEventsInput, '10');
+        });
+        // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+        expect(container.querySelector('#event-list').children.length).toBe(10);
     });
 });
