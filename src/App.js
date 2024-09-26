e7416d77f146b98048ca0c4950769e0fdc59245b
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-26 11:45:31 +0200

    I added a loading screen and converted the html to react-bootstrap components.

--- src/App.js
+++ src/App.js
@@ -5,28 +5,47 @@ import { getEvents, extractLocations } from './api';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Stack from 'react-bootstrap/Stack';
 import NumberOfEvents from './components/NumberOfEvents';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 function App() {
     const [events, setEvents] = useState([]);
     const [numberOfEvents, setNumberOfEvents] = useState(32);
-    useEffect(() => { (async () => setEvents(await getEvents()))(); }, []);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        (async () => {
+            setEvents(await getEvents());
+            setLoading(false);
+        })();
+    }, []);
+    if (loading) {
+        return <div>Loading...</div>;
+    }
     return (
-        <Stack gap={1} className="App container d-flex align-items-center w-100">
-            <nav className='navbar navbar-expand-lg bg-body-tertiary'>
-                <div className='container-fluid'>
-                    <a className='navbar-brand' href='/'>Meet App</a>
-                    <CitySearch allLocations={extractLocations(events)} />
-                    <NumberOfEvents updateEvents={(number) => {
-                        if (number > 0 && number < events.length) {
-                            setNumberOfEvents(number);
-                        } else {
-                            setNumberOfEvents(32);
-                        }
-                    }} max={events.length} placeholder={32} />
-                </div>
-            </nav>
-            <EventList events={events} numberOfEvents={numberOfEvents} />
-        </Stack>
+        <Container>
+            <Stack gap={1} className="d-flex align-items-center">
+                <Navbar bg="light" expand="lg" className='w-100'>
+                    <Container fluid className='w-100'>
+                        <Navbar.Brand href="/">Meet App</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbar-nav" />
+                        <Navbar.Collapse id="navbar-nav">
+                            <Nav className="me-auto">
+                                <CitySearch allLocations={extractLocations(events)} />
+                            </Nav>
+                            <Nav>
+                                <NumberOfEvents updateEvents={(number) => {
+                                    if (number > 0 && number < events.length) {
+                                        setNumberOfEvents(number);
+                                    } else {
+                                        setNumberOfEvents(32);
+                                    }
+                                }} max={events.length} placeholder={32} />
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                <EventList events={events} numberOfEvents={numberOfEvents} className='w-100' />
+            </Stack>
+        </Container>
     );
 }
 
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,28 +1,28 @@
 // src/__tests__/App.test.js
 
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 
 import App from '../App';
-import React, { act } from 'react';
+import React from 'react';
 
 describe('<App /> component', () => {
     test('renders list of events', async () => {
         const { container } = render(<App />);
-        await act(async () => {
+        await waitFor(() => {
             // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
             expect(container.querySelector('#event-list')).toBeInTheDocument();
         });
     });
     test('render CitySearch', async () => {
         const { container } = render(<App />);
-        await act(async () => {
+        await waitFor(() => {
             // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
             expect(container.querySelector('#city-search')).toBeInTheDocument();
         });
     });
     test('render NumberOfEvents', async () => {
         const { container } = render(<App />);
-        await act(async () => {
+        await waitFor(() => {
             // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
             expect(container.querySelector('#numberOfEvents')).toBeInTheDocument();
         });
