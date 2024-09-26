497aa8284531cf22f736be3969b78d77c6ea6613
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-26 19:17:38 +0200

    Changed wording

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,21 +6,21 @@ import App from '../App';
 import React from 'react';
 
 describe('<App /> component', () => {
-    test('renders list of events', async () => {
+    test('shows list of events', async () => {
         const { container } = render(<App />);
         await waitFor(() => {
             // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
             expect(container.querySelector('#event-list')).toBeInTheDocument();
         });
     });
-    test('render CitySearch', async () => {
+    test('shows CitySearch input', async () => {
         const { container } = render(<App />);
         await waitFor(() => {
             // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
             expect(container.querySelector('#city-search')).toBeInTheDocument();
         });
     });
-    test('render NumberOfEvents', async () => {
+    test('shows NumberOfEvents input', async () => {
         const { container } = render(<App />);
         await waitFor(() => {
             // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
