f9f7d36869944f90109526ab1d81e2afe27ba06f
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-27 16:56:44 +0200

    Added a integration test for the CitySearch

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,10 @@
 // src/__tests__/App.test.js
 
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, within } from '@testing-library/react';
 
 import App from '../App';
-import React from 'react';
+import React, { act } from 'react';
+import userEvent from '@testing-library/user-event';
 
 describe('<App /> component', () => {
     test('shows list of events', async () => {
@@ -30,5 +31,27 @@ describe('<App /> component', () => {
             numberOfEvents = screen.getByLabelText('Events on page:')
         });
         expect(numberOfEvents).toBeInTheDocument();
+    });
+});
+
+describe('<App /> integration', () => {
+    test('renders a list of events matching the city selected by the user', async () => {
+        render(<App />);
+        const user = userEvent.setup();
+        let citySearchInput, citySearchList, eventList;
+        await waitFor(() => {
+            citySearchInput = screen.getByPlaceholderText('Search for a city');
+            const _lists = screen.getAllByRole('list');
+            citySearchList = _lists[0];
+            eventList = _lists[1];
+        });
+        expect(citySearchList).toHaveClass('no-caret nav-item dropdown');
+        expect(eventList).toHaveAttribute('id', 'event-list');
+        await act(async () => { await user.type(citySearchInput, 'Berlin') });
+        const berlinSuggestionItem = within(citySearchList).queryByText('Berlin, Germany');
+        expect(berlinSuggestionItem).toBeInTheDocument();
+        await act(async () => { await user.click(berlinSuggestionItem) });
+        const eventListItems = within(eventList).getAllByRole('listitem');
+        expect(eventListItems.every(item => item.textContent.includes('Berlin'))).toBe(true);
     });
 });
