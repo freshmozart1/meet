4143363cccdbd5b8d2b3a57d244f82f17ab857b6
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-27 15:46:46 +0200

    moved setLoading to its own useEffect hook, because it depends on events and therefore should be executed after setEvents. I also added a new locations state and added setLocations to the same useEffect hook as setLoading.

--- src/App.js
+++ src/App.js
@@ -11,40 +11,47 @@ function App() {
     const defaultNOE = 32;
     const [events, setEvents] = useState([]);
     const [currentNOE, setCurrentNOE] = useState(defaultNOE);
+    const [locations, setLocations] = useState([]);
     const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         (async () => {
             setEvents(await getEvents());
-            setLoading(false);
         })();
     }, []);
 
+    useEffect(() => {
+        setLocations(extractLocations(events));
+        setLoading(events.length === 0);
+    }, [events]);
+
     if (loading) {
         return <div>Loading...</div>;
+    } else {
+        return (
+            <Container>
+                <Stack gap={1} className="d-flex align-items-center">
+                    <Navbar bg="light" expand="lg" className='w-100'>
+                        <Container fluid className='w-100'>
+                            <Navbar.Brand href="/">Meet App</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="navbar-nav" />
+                            <Navbar.Collapse id="navbar-nav">
+                                <Nav className="me-auto">
+                                    <CitySearch allLocations={locations} />
+                                </Nav>
+                                <Nav>
+                                    <NumberOfEvents updateEvents={(number) => {
+                                        setCurrentNOE(number);
+                                    }} max={events.length} defaultValue={defaultNOE} />
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                    <EventList events={events.slice(0, currentNOE)} className='w-100' />
+                </Stack>
+            </Container>
+        );
     }
-    return (
-        <Container>
-            <Stack gap={1} className="d-flex align-items-center">
-                <Navbar bg="light" expand="lg" className='w-100'>
-                    <Container fluid className='w-100'>
-                        <Navbar.Brand href="/">Meet App</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="navbar-nav" />
-                        <Navbar.Collapse id="navbar-nav">
-                            <Nav className="me-auto">
-                                <CitySearch allLocations={extractLocations(events)} />
-                            </Nav>
-                            <Nav>
-                                <NumberOfEvents updateEvents={(number) => {
-                                    setCurrentNOE(number);
-                                }} max={events.length} defaultValue={defaultNOE} />
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-                <EventList events={events.slice(0, currentNOE)} className='w-100' />
-            </Stack>
-        </Container>
-    );
 }
 
 export default App;
