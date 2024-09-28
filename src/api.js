68c488a0980d8c4c8ca5e0bac439415d3655d311
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-28 19:40:37 +0200

    result.events was undefined

--- src/api.js
+++ src/api.js
@@ -53,13 +53,12 @@ export const extractLocations = (events) => {
 };
 
 export const getEvents = async () => {
-    /*if (window.location.href.startsWith('http://localhost')) {
+    if (window.location.href.startsWith('http://localhost')) {
         return mockEvents;
-    }*/
+    }
     const token = await getAccessToken();
     removeQuery();
     const response = await fetch('https://540vqkhysf.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/' + token);
     const result = await response.json();
-    console.log(result);
-    if (result) { return result.events; } else return null;
+    return result ? result : null;
 }
